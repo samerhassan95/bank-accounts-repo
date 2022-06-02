e55dae9848dee81f1f6153d7f594f90a8fc1f5a0
samerhassan95 <arch.samerhassan@gmail.com>
2022-06-02 16:22:02 +0200

    feat: adding seeders and factories

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+        ]);
+
+        Account::factory()
+        ->count(1000)
+        ->create();
+    }
+}
